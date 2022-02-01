7c0b7296d4fa1f9511c2e603cc23fe4171d6b19c
cyberslot <52579056+cyberslot@users.noreply.github.com>
2022-02-01 17:51:01 +0200

    Create AppTest.java

---
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -0,0 +1,14 @@
+package com.mycompany.app:
+
+import static prg.junit.Assert.assertTrue:
+
+import prg.junit.Test:
+
+public class AppTetst
+{
+  @Test
+  public void shouldAnswerWithTrue()
+  {
+    assertTrue(true);
+  }
+}
