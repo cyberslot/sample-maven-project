a0918b8cd49de741203bafd1c76e7a5522b51e1b
cyberslot <52579056+cyberslot@users.noreply.github.com>
2022-02-02 11:03:13 +0200

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,8 +1,8 @@
-package com.mycompany.app:
+package com.mycompany.app;
 
-import static org.junit.Assert.assertTrue:
+import static org.junit.Assert.assertTrue;
 
-import org.junit.Test:
+import org.junit.Test;
 
 public class AppTetst
 {
